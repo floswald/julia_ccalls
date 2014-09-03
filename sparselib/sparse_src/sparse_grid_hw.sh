9ba86208bdf8844405a98c7c958ea9d0cd908a55
Florian Oswald <florian.oswald@gmail.com>
2014-09-03 14:42:52 +0100

    added more to compilation

--- sparselib/sparse_src/sparse_grid_hw.sh
+++ sparselib/sparse_src/sparse_grid_hw.sh
@@ -2,6 +2,8 @@
 # on macos:
 clang -fpic -shared -undefined dynamic_lookup -I. sparse_grid_hw.c -o sparselib.dylib
 
-# then symlink into /usr/local/lib
+# then symlink into /usr/local/lib or similiar:
+# ln -s /Users/yourusername/git/julia_ccalls/sparselib/sparse_src/sparselib.dylib /usr/local/lib/sparselib.dylib
+
 # then in julia
 # ccall((:cc_weights,"sparselib"), Ptr{Float64}, (Int,),n)
